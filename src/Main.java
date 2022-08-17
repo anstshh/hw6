0299ff82b713436fbf600ad61034edbff65150ad
Admin <anstdor@gmail.com>
2022-08-17 16:55:00 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //Задание 1
+
+        String firstName = " Ivan";
+        String middleName = " Ivanovich";
+        String lastName = " Ivanov";
+        String fullName = firstName + middleName + lastName;
+        System.out.println("ФИО сотрудника — " + fullName);
+
+        //Задание 2
+
+
+
+
+
+    }
+}
