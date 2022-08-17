0635d2d20bd745cd06edd92c56a07907f1dc0507
Admin <anstdor@gmail.com>
2022-08-17 17:00:35 +0300

    Middle commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        //Задание 1
+        //Задание 1.
 
         String firstName = " Ivan";
         String middleName = " Ivanovich";
@@ -9,7 +9,17 @@ public class Main {
         String fullName = firstName + middleName + lastName;
         System.out.println("ФИО сотрудника — " + fullName);
 
-        //Задание 2
+        //Задание 2.
+
+        String firstName1 = " Ivan";
+        String middleName1 = " Ivanovich";
+        String lastName1 = " Ivanov";
+        String fullName1 = firstName1 + middleName1 + lastName1;
+        String fullName2 = fullName1.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName2);
+
+
+
 
 
 
